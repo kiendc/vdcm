71f7191bce64d90d4c3d23e9f311103ad3f615b4
kiendc <kiendc@gmail.com>
2018-03-11 22:24:38 -0400

    Add script and css for datatable-bootstrap

--- com_vdcm/site/views/vjdcm/tmpl/default.php
+++ com_vdcm/site/views/vjdcm/tmpl/default.php
@@ -4,7 +4,8 @@ JHtml::_('bootstrap.framework');
 
 $doc =  JFactory::getDocument();
 $doc->addStyleSheet('components/com_vjeecdcm/css/vjeecdcm.css');
-
+$doc->addStyleSheet('https://cdn.datatables.net/v/bs/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/datatables.min.css');
+$doc->addScript('https://cdn.datatables.net/v/bs/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/datatables.min.js');
 ?>
 
 
