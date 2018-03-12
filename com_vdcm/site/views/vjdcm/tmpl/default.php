e6be1db664a08baeb54e6e4470bf37fd494f9de1
kiendc <kiendc@gmail.com>
2018-03-11 22:35:12 -0400

    Try an example of datatable-bootstrap

--- com_vdcm/site/views/vjdcm/tmpl/default.php
+++ com_vdcm/site/views/vjdcm/tmpl/default.php
@@ -8,5 +8,35 @@ $doc->addStyleSheet('https://cdn.datatables.net/v/bs/dt-1.10.16/af-2.2.2/b-1.5.1
 $doc->addScript('https://cdn.datatables.net/v/bs/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/datatables.min.js');
 ?>
 
+<script type="text/javascript">
+ 	// set site_url variable which to be used in create_request.js file 
+	var site_url = "<?php echo JURI::root(); ?>";
+  $(document).ready(function() {
+    $('#example').DataTable();
+} );
+</script>
 
-
+<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+        <tbody>
+  </tbody>
+</table>
