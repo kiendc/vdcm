57318dc6c5c11904b5d4a2fbe74c0151d012efbb
kiendc <kiendc@gmail.com>
2018-03-15 09:16:03 -0400

    create UI for employee. Leave default layout as an entry point

--- com_vdcm/site/views/vjdcm/tmpl/default.php
+++ com_vdcm/site/views/vjdcm/tmpl/default.php
@@ -1,35 +1,4 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
-
-JHtml::_('jquery.framework', false);
-$doc =  JFactory::getDocument();
-
-$doc->addStyleSheet('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
-$doc->addStyleSheet('https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css');
-
-$doc->addScript('https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js');
-$doc->addScript('https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js');
 ?>
 
-<script type="text/javascript">
- 	// set site_url variable which to be used in create_request.js file 
-	var site_url = "<?php echo JURI::root(); ?>";
-	$(document).ready(function(){
-		$('#example').DataTable();
-	});
-</script>
-
-<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-  </tbody>
-</table>
