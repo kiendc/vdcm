edb3a2e81b61f488df9e6e61b75778b9430c0b9f
vjeecvn <vjeecvn@a2plcpnl0541.prod.iad2.secureserver.net>
2018-03-30 15:20:22 -0700

    worked tab with datatable and checkbox

--- com_vdcm/site/js/client.js
+++ com_vdcm/site/js/client.js
@@ -1,50 +1,66 @@
+function activateTab(tab)
+{
+    console.log('activate ' + tab); 
+    $('#main-client-tab a[href="#' + tab + '"]').tab('show');
+}
+
+function onTabActivated(e)
+{
+    //console.log('New tab' + e.target);
+    //console.log(e.relatedTarget);
+}
+
+function showRequestTable(data)
+{
+    $('#example').DataTable({
+       data: data,
+       columns : [
+	   {data: 'request_id', orderable: false},	
+           {data: 'code'},
+           {data: 'created_date'},
+           {data: 'holder_name'},
+           {data: 'degree_name'},
+           {data: 'route'},
+           {data: 'name'},
+           {data: 'begin_date'}
+       ],
+       columnDefs: [{
+	   targets: 0,
+           render: function (data, type, full, meta){
+               return '<input type="checkbox" name="id[]" value="' + data + '">';
+           }
+       }],
+    });
+    //activateTab('taba');
+    //$('main-client-tab a:first').tab('show');
+}
+
+function getRequest()
+{
+     var url = document.location.origin + "/index.php?option=com_vjeecdcm&task=client.getRequests";
+     data = {};
+     data[frmTk] = 1;
+     console.log(url);
+
+     $.ajax({
+         url: url,
+         data: data,
+         dataType: "json",
+         type: 'POST',
+         error: function (jqXHR, textStatus, errorThrown)
+         {
+             alert(errorThrown);
+         },
+         success: function (data, textStatus, jqXHR)
+         {
+             console.log(data.requests);
+	     showRequestTable(data.requests);
+         }
+     });   
+}
+
 $(document).ready(function(){
-/*  
-$('#example').DataTable({
-    "processing": true,
-    "serverSide": true,
-    "ajax": {
-      "url": "http://dev.vjeec.org/index.php?option=com_vjeedcm&task=client.getRequests",
-      "type": "POST"
-    },
-    "columns": [
-      { "data": "code" },
-      { "data": "name" },
-      { "data": "file" },
-      { "data": "school" },
-      { "data": "status" },
-      { "data": "update_date" }
-    ]
-  });
-*/
-	var url = document.location.origin + "/index.php?option=com_vjeecdcm&task=client.getRequests";
-	data = {};
-	data[frmTk] = 1;
-	console.log(url);
-	$.ajax({
-		url: url,
-		data: data,
-		dataType: "json",
-		type: 'POST',
-		error: function (jqXHR, textStatus, errorThrown)
-		{
-		    		alert(errorThrown);
-		},
-		success: function (data, textStatus, jqXHR) 
-		{
-			console.log(data.requests);
-			$('#example').DataTable({
-				data: data.requests,
-				columns : [
-					{data: 'code'},
-					{data: 'created_date'},
-					{data: 'holder_name'},
-					{data: 'degree_name'},
-					{data: 'route'},
-					{data: 'name'},
-					{data: 'begin_date'}
-				]
-			});
-		}
-	});
+   $('a[data-toggle="tab"]').on('show.bs.tab', onTabActivated);
+   getRequest();	
+	
 });
--- com_vdcm/site/views/vjdcm/tmpl/client.php
+++ com_vdcm/site/views/vjdcm/tmpl/client.php
@@ -6,6 +6,7 @@ $doc =  JFactory::getDocument();
 
 //$doc->addStyleSheet('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
 //$doc->addStyleSheet('https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css');
+$doc->addScript('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
 
 $doc->addScript('https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js');
 $doc->addScript('https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js');
@@ -14,26 +15,25 @@ $doc->addScript('components/com_vjeecdcm/js/vjdcm.js');
 $doc->addScript('components/com_vjeecdcm/js/client.js');
 ?>
 
-<ul class="nav nav-tabs">
-  <li role="presentation" class="active"><a href="#">Dashboard</a></li>
-  <li role="presentation"><a href="#">Requests</a></li>
-  <li role="presentation"><a href="#">Messages</a></li>
+<ul class="nav nav-tabs" id="main-client-tab">
+  <li class="active"><a href="#taba" data-toggle="tab">Dashboard</a></li>
+  <li><a href="#tabb" data-toggle="tab">Requests</a></li>
+  <li><a href="#tabc" data-toggle="tab">Messages</a></li>
 </ul>
 <br>
-<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-        <thead>
-            <tr>
-<?php
-    echo '<th>' . JText::_( 'VJEECDCM_RV_CT_REQUEST_CODE' ) . '</th>';
-    echo '<th>' . JText::_( 'VJEECDCM_RV_CT_REQUEST_CREATED_DATE' ) . '</th>';
-    echo '<th>' . JText::_( 'VJEECDCM_RV_CT_DIPLOMA_HOLDER_TITLE' ) . '</th>';
-    echo '<th>' . JText::_( 'VJEECDCM_RV_CT_DIPLOMA_TITLE' ) . '</th>';
-    echo '<th>' . JText::_( 'VJEECDCM_RV_CT_REQUEST_SCHOOL' ) . '</th>';
-    echo '<th>' . JText::_( 'VJEECDCM_RV_CT_REQUEST_PROCESS_TITLE' ) . '</th>';
-    echo '<th>' . JText::_( 'VJEECDCM_RV_CT_REQUEST_LAST_UPDATE_DATE' ) . '</th>';
+<div class="tab-content" id="tabs">
+<div class="tab-pane active" id="taba">
+<?php 
+echo "Tab a";
 ?>
-            </tr>
-        </thead>
-        <tbody>
-  </tbody>
-</table>
+</div>
+<div class="tab-pane" id="tabb">
+
+<?php
+echo $this->loadTemplate('tab-content-1');
+?>
+</div>
+<div class="tab-pane" id="tabc">
+Tab content 3
+</div>
+</div>
