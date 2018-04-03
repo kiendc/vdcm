0090628b72441b40254c5aeecfedaad69c5e8d20
Cong-Kien Dang <kiendc@gmail.com>
2018-04-02 23:23:43 -0400

    Refine the creation form

--- com_vdcm/site/views/vjdcm/tmpl/client_req-create-dlg.php
+++ com_vdcm/site/views/vjdcm/tmpl/client_req-create-dlg.php
@@ -9,44 +9,36 @@
       </div>
       <div class="modal-body">
 				<form class="form-inline">
-					<div>
-						<div class="detail-sec" id="req-detail-sec-1">
-							<h3>Thong tin </h3>
-              <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <a href="#" class="form-control" id="exampleInputEmail1"placeholder="Enter email">
-              </div>
-							<div>
-								<div class="form-group">
-									<label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_SCHOOL' ); ?></label>
-									<a href="#" id="req-detail-target-school" data-type="text" data-pk="1" data-placement="right" data-placeholder="Required" data-title="Enter your firstname"></a>
-								</div>
-								<div class="form-group">
-									<label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_TYPE' ); ?></label>
-									<span class="req-editable" id="req-detail-type"></span>
-									</div>
-							</div>
+          <div class="detail-sec" id="req-detail-sec-1">
+            <h3>Thong tin </h3>
+            <p class="form-group">
+            <label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_SCHOOL' ); ?></label>
+            <a href="#" id="req-detail-target-school" data-type="text" data-pk="1" data-placement="right" data-placeholder="Required" data-title="Enter your firstname"></a>
+            </p>
+            <p class="form-group">
+            <label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_TYPE' ); ?></label>
+            <span class="req-editable" id="req-detail-type"></span>
+            </p>
           </div>
           <div class="detail-sec" id="req-detail-sec-2">
             <h3>Ho so </h3>
             <div>
               <input type="hidden" id="req-detail-dplm-id" value="0"/>
               <p><label><?php echo JText::_( 'VJEECDCM_RV_CT_DIPLOMA_TITLE' ); ?></label>
-              <span class='dplm-editable' id='req-detail-diploma'></span></p>
+              <a href="#" id='req-detail-diploma'></span></p>
               <p><label><?php echo JText::_( 'VJEECDCM_RV_CT_DIPLOMA_HOLDER_TITLE' ); ?></label>
-              <span class="dplm-editable editable-txt" id='req-detail-holder'></span></p>
+              <a href="#" class="dplm-editable editable-txt" id='req-detail-holder'></span></p>
               <p><label><?php echo JText::_( 'VJEECDCM_DIPLOMA_ADD_FORM_FS_HOLDER_ID_NUMBER' ); ?></label>
-              <span class="dplm-editable editable-txt" id="req-detail-holder-info-1"></span>
+              <a href="#" class="dplm-editable editable-txt" id="req-detail-holder-info-1"></span>
               <label><?php echo JText::_( 'VJEECDCM_DIPLOMA_ADD_FORM_FS_HOLDER_BIRTHDAY' ); ?></label>
-              <span class='dplm-editable' id="req-detail-holder-info-4"></span></p>
+              <a href="#" id="req-detail-holder-info-4"></span></p>
               <p><label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_CONTACT_TELEPHONE' ); ?></label>
-              <span class='dplm-editable editable-txt' id="req-detail-holder-info-2"></span></p>
+              <a href="#" class='dplm-editable editable-txt' id="req-detail-holder-info-2"></span></p>
               <p><label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_CONTACT_ADDRESS' ); ?></label>
-              <span class='dplm-editable' id="req-detail-holder-info-3"></span></p>
+              <a href="#" class='dplm-editable' id="req-detail-holder-info-3"></span></p>
             </div>
           </div>
           <input type="hidden" id="upload-path" value=""/>
-        </div>
 				<?php echo JHtml::_('form.token'); ?>
 				</form>
       </div>
