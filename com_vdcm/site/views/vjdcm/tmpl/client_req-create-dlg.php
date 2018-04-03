1068f5a2691ed47f08983c59bc6b419e5d4b5e5d
Cong-Kien Dang <kiendc@gmail.com>
2018-04-02 23:27:00 -0400

    replace all editable-span to editable-a

--- com_vdcm/site/views/vjdcm/tmpl/client_req-create-dlg.php
+++ com_vdcm/site/views/vjdcm/tmpl/client_req-create-dlg.php
@@ -11,13 +11,13 @@
 				<form class="form-inline">
           <div class="detail-sec" id="req-detail-sec-1">
             <h3>Thong tin </h3>
-            <p class="form-group">
+            <p>
             <label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_SCHOOL' ); ?></label>
             <a href="#" id="req-detail-target-school" data-type="text" data-pk="1" data-placement="right" data-placeholder="Required" data-title="Enter your firstname"></a>
             </p>
-            <p class="form-group">
+            <p>
             <label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_TYPE' ); ?></label>
-            <span class="req-editable" id="req-detail-type"></span>
+            <a href="#" id="req-detail-type"></a>
             </p>
           </div>
           <div class="detail-sec" id="req-detail-sec-2">
@@ -25,17 +25,17 @@
             <div>
               <input type="hidden" id="req-detail-dplm-id" value="0"/>
               <p><label><?php echo JText::_( 'VJEECDCM_RV_CT_DIPLOMA_TITLE' ); ?></label>
-              <a href="#" id='req-detail-diploma'></span></p>
+              <a href="#" id='req-detail-diploma'></a></p>
               <p><label><?php echo JText::_( 'VJEECDCM_RV_CT_DIPLOMA_HOLDER_TITLE' ); ?></label>
-              <a href="#" class="dplm-editable editable-txt" id='req-detail-holder'></span></p>
+              <a href="#" class="dplm-editable editable-txt" id='req-detail-holder'></a></p>
               <p><label><?php echo JText::_( 'VJEECDCM_DIPLOMA_ADD_FORM_FS_HOLDER_ID_NUMBER' ); ?></label>
-              <a href="#" class="dplm-editable editable-txt" id="req-detail-holder-info-1"></span>
+              <a href="#" class="dplm-editable editable-txt" id="req-detail-holder-info-1"></a>
               <label><?php echo JText::_( 'VJEECDCM_DIPLOMA_ADD_FORM_FS_HOLDER_BIRTHDAY' ); ?></label>
-              <a href="#" id="req-detail-holder-info-4"></span></p>
+              <a href="#" id="req-detail-holder-info-4"></a></p>
               <p><label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_CONTACT_TELEPHONE' ); ?></label>
-              <a href="#" class='dplm-editable editable-txt' id="req-detail-holder-info-2"></span></p>
+              <a href="#" class='dplm-editable editable-txt' id="req-detail-holder-info-2"></a></p>
               <p><label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_CONTACT_ADDRESS' ); ?></label>
-              <a href="#" class='dplm-editable' id="req-detail-holder-info-3"></span></p>
+              <a href="#" class='dplm-editable' id="req-detail-holder-info-3"></a></p>
             </div>
           </div>
           <input type="hidden" id="upload-path" value=""/>
