887a60668f67762a0e70115d8b1d9c85b7860b0c
Kien Dang <kiendc@gmail.com>
2018-05-01 23:00:25 -0400

    Use string constant for section title in add request form

--- com_vdcm/site/views/vjdcm/tmpl/client_req-create-dlg.php
+++ com_vdcm/site/views/vjdcm/tmpl/client_req-create-dlg.php
@@ -8,24 +8,23 @@
         <h4 class="modal-title" id="gridSystemModalLabel">Modal title</h4>
       </div>
       <div class="modal-body">
-				<form class="form-inline">
+        <form class="form-inline">
           <div class="detail-sec" id="req-detail-sec-1">
-            <h3>Thong tin </h3>
-            <p>
-            <label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_SCHOOL' ); ?></label>
-            <a href="#" id="req-detail-target-school" data-type="text" data-pk="1" data-placement="right" data-placeholder="Required" data-title="Enter your firstname"></a>
-            </p>
+            <h3><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION' ); ?></h3>
+                <input type="hidden" id="req-detail-dplm-id" value="0"/>
+                <p><label><?php echo JText::_( 'VJEECDCM_RV_CT_DIPLOMA_TITLE' ); ?></label>
+                <a href="#" id='req-detail-diploma'></a></p>
+                <p><label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_SCHOOL' ); ?></label>
+                <a href="#" id="req-detail-target-school" data-type="text" data-pk="1" data-placement="right" data-placeholder="Required" data-title="Enter your firstname"></a>
+                </p>
             <p>
             <label><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_INFORMATION_TYPE' ); ?></label>
             <a href="#" id="req-detail-type"></a>
             </p>
           </div>
           <div class="detail-sec" id="req-detail-sec-2">
-            <h3>Ho so </h3>
+            <h3><?php echo JText::_( 'VJEECDCM_REQUEST_ADD_FORM_FS_CONTACT' ); ?></h3>
             <div>
-              <input type="hidden" id="req-detail-dplm-id" value="0"/>
-              <p><label><?php echo JText::_( 'VJEECDCM_RV_CT_DIPLOMA_TITLE' ); ?></label>
-              <a href="#" id='req-detail-diploma'></a></p>
               <p><label><?php echo JText::_( 'VJEECDCM_RV_CT_DIPLOMA_HOLDER_TITLE' ); ?></label>
               <a href="#" class="dplm-editable editable-txt" id='req-detail-holder'></a></p>
               <p><label><?php echo JText::_( 'VJEECDCM_DIPLOMA_ADD_FORM_FS_HOLDER_ID_NUMBER' ); ?></label>
