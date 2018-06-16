82e9d784249203809458bc86bff93caf2a4f0da6
Kien Dang <kiendc@gmail.com>
2018-06-16 14:20:59 -0400

    Initialize employee.js

---
+++ com_vdcm/site/js/employee.js
@@ -0,0 +1,8 @@
+
+function onDocumentReady()
+{
+    $.fn.editable.defaults.mode = 'inline';
+    $('a[data-toggle="tab"]').on('show.bs.tab', onTabActivated);
+}
+
+$(document).ready(onDocumentReady);
