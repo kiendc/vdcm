cb2a235189a09e22f204a6da8eb4a324ca01da54
Kien Dang <kiendc@gmail.com>
2018-06-17 00:28:51 -0400

    Continue simplifying content empl-req-tab

--- com_vdcm/site/views/vjdcm/tmpl/employee_tab-content-req.php
+++ com_vdcm/site/views/vjdcm/tmpl/employee_tab-content-req.php
@@ -49,36 +49,6 @@
             <!-- Tab panes -->
             <div class="tab-content">
                 <div class="tab-pane fade in active" id="home">
-                    <div class="list-group">
-                        <a href="#" class="list-group-item">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox">
-                                        </label>
-                            </div>
-                            <span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;
-                                display: inline-block;">Mark Otto</span> <span class="">Nice work on the lastest version</span>
-                            <span class="text-muted" style="font-size: 11px;">- More content here</span> <span class="badge">12:10 AM</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip">
-                            </span></span></a><a href="#" class="list-group-item">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox">
-                                            </label>
-                                </div>
-                                <span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;
-                                    display: inline-block;">Jason Markus</span> <span class="">This is big title</span>
-                                <span class="text-muted" style="font-size: 11px;">- I saw that you had..</span> <span class="badge">12:09 AM</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip">
-                                </span></span></a><a href="#" class="list-group-item read">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox">
-                                                </label>
-                                    </div>
-                                    <span class="glyphicon glyphicon-star"></span><span class="name" style="min-width: 120px;
-                                        display: inline-block;">Jane Patel</span> <span class="">This is big title</span>
-                                    <span class="text-muted" style="font-size: 11px;">- Hi hello how r u ?</span> <span class="badge">11:30 PM</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip">
-                                    </span></span></a>
-                    </div>
                 </div>
                 <div class="tab-pane fade in" id="profile">
                     <div class="list-group">
@@ -93,13 +63,6 @@
                     This tab is empty.</div>
             </div>
             
-            <div class="row-md-12">
-                
-                <div class="well">
-                    <a href="http://www.bootply.com/XXmcPas41w">Edit on Bootply</a>
-                </div>
-                
-            </div>
         </div>
     </div>
 </div>
