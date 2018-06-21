ab3a28cf090adb2de4b3e352224e40b6e7a81618
Kien Dang <kiendc@gmail.com>
2018-06-20 22:59:28 -0400

    Use table.ajax option to reload tables

--- com_vdcm/site/js/employee.js
+++ com_vdcm/site/js/employee.js
@@ -1,28 +1,43 @@
+function initializedTables()
+{
+    var url = document.location.origin + "/index.php?option=com_vjeecdcm&task=reqemployee.getRequests";
+    var data = {};
+    data[frmTk] = 1;
+    data["step"] = 2;
+    submTabl = $('#recv-reqs-table').DataTable({
+                                               ajax: {
+                                                url: url,
+                                                type: 'POST',
+                                                data: data,
+                                               dataSrc: function(servResp){
+                                                console.log(servResp);
+                                                return servResp.requests);
+                                               }
+                                               },
+                                               columns :
+                                               [
+                                                {data: 'request_id', orderable: false},
+                                                {data: 'code'},
+                                                {data: 'created_date'},
+                                                {data: 'holder_name'},
+                                                {data: 'degree_name'},
+                                                {data: 'route'}
+                                                ],
+                                               columnDefs:
+                                               [{
+                                                targets: 0,
+                                                render: function (data, type, full, meta)
+                                                {
+                                                return '<input type="checkbox" name="id[]" value="' + data + '">';
+                                                }
+                                                }],
+                                               });
+}
 function showRequestTable(step, data)
 {
     if (step == 2)
     {
-        $('#recv-reqs-table').DataTable(
-                                        {
-                                        data: data,
-                                        columns :
-                                        [
-                                         {data: 'request_id', orderable: false},
-                                         {data: 'code'},
-                                         {data: 'created_date'},
-                                         {data: 'holder_name'},
-                                         {data: 'degree_name'},
-                                         {data: 'route'}
-                                         ],
-                                        columnDefs:
-                                        [{
-                                         targets: 0,
-                                         render: function (data, type, full, meta)
-                                         {
-                                         return '<input type="checkbox" name="id[]" value="' + data + '">';
-                                         }
-                                         }],
-                                        });
+        
         return;
     }
     //activateTab('taba');
@@ -31,6 +46,7 @@ function showRequestTable(step, data)
 
 function getRequest(step)
 {
+    /*
     var url = document.location.origin + "/index.php?option=com_vjeecdcm&task=reqemployee.getRequests";
     var data = {};
     data[frmTk] = 1;
@@ -52,6 +68,9 @@ function getRequest(step)
            showRequestTable(step, data.requests);
            }
            });
+     */
+    if (step == 2)
+        submTable.ajax.reload(null, false);
 }
 
 function onTabActivated(e)
@@ -63,6 +82,7 @@ function onTabActivated(e)
 function onDocumentReady()
 {
     $.fn.editable.defaults.mode = 'inline';
+    initializedTables();
     $('a[data-toggle="tab"]').on('show.bs.tab', onTabActivated);
 }
 
